864448cc33f50debca4befc63beec339065531b6
Naman <139859234+ss-2303@users.noreply.github.com>
2026-04-02 16:12:57 +1100

    url fixed

--- researchmind-app/backend/main.py
+++ researchmind-app/backend/main.py
@@ -30,7 +30,7 @@ app = FastAPI(title="ResearchMind API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174","http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5174","http://localhost:5173", "http://localhost:3000", "https://deep-agent-research-mind.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
